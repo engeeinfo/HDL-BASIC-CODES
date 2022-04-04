93472e8c8516fad26aad9d6cca56162eca35f298
engeeinfo <101986991+engeeinfo@users.noreply.github.com>
2022-04-04 21:06:02 +0530

    Add files via upload

---
+++ FULL ADDER/full_adder.v
@@ -0,0 +1,9 @@
+module full_adder(sum,carry,a,b,c);
+
+input wire a,b,c;
+output wire sum;
+output carry;
+
+assign sum=a^b^c;
+assign carry=b*c+a*b+c*a;
+endmodule 
---
+++ FULL ADDER/full_adder_tb.v
@@ -0,0 +1,51 @@
+
+module half_adder_tb;
+reg a,b,c;
+wire sum,carry;
+half_adder uut (.a(a),.b(b),.c(c),.sum(sum),.carry(carry));
+
+initial
+begin
+$monitor($time,"a=%b b=%b c=%b sum=%b carry=%b",a,b,c,sum,carry);
+a=0;
+b=0;
+c=0;
+#10; 
+
+a=1;
+b=0;
+c=0;
+#10;
+
+a=0;
+b=1;
+c=0;
+#10;
+
+a=0;
+b=0;
+c=1;
+#10;
+
+a=1;
+b=0;
+c=1;
+#10;
+
+a=1;
+b=1;
+c=0;
+#10;
+
+a=1;
+b=1;
+c=1;
+#10;
+
+a=0;
+b=1;
+c=1;
+#10;
+
+end
+endmodule 
